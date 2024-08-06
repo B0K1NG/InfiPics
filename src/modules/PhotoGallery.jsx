0ba303b7a9f0d19126469d3f0b1c7442c5a8edbe
B0K1NG <dominykaspavlijus@gmail.com>
2024-08-06 18:01:26 +0300

    Fixed Photo Gallery to have all of the content that is needed regarding the task

--- src/modules/PhotoGallery.jsx
+++ src/modules/PhotoGallery.jsx
@@ -6,13 +6,18 @@ function PicsGallery({ photos = [] }) {
         <div className="photo-gallery">
             {photos.map(photo => (
                     <div key={photo.id} className="photo-gallery__item">
-                        <img className="photo-gallery__image"
+                    <img
+                            className="photo-gallery__image"
                             src={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_w.jpg`}
                             alt={photo.title}
-                        />
-                    <p className="photo-gallery__title">{photo.title}</p>
+                    />
+                    <div className="overlay-content">
+                        <p className="photo-gallery__title">{photo.title}</p>
+                        <div className="line"></div>
+                        <p className="author">{photo.author}</p>
                     <FavoriteButton itemId={photo.id}/>
                     </div>
+                </div>
             ))}
         </div>
     );
